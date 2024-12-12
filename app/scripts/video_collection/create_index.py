a0c51dd1af458c47a9aa63948ed131617a7b328b
zhangchenchen <zhangchenchen@51aes.com>
2024-12-12 10:35:22 +0800

    调整索引名称

--- app/scripts/video_collection/create_index.py
+++ app/scripts/video_collection/create_index.py
@@ -16,7 +16,7 @@ def create_index():
         field_name="embedding",
         metric_type="IP",
         index_type="IVF_FLAT",
-        index_name="vector_index",
+        index_name="embedding_index",
         params={"nlist": 512}
     )
 
@@ -24,7 +24,7 @@ def create_index():
         field_name="summary_embedding",
         metric_type="IP",
         index_type="IVF_FLAT",
-        index_name="vector_index",
+        index_name="summary_embedding_index",
         params={"nlist": 512}
     )
 
